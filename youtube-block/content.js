4e0fe70eec7952830fc5e15994e8312ccda88693
zhuo.liu <zhuo.liu@xxx.com>
2025-04-14 10:45:07 +0800

    fix:新增供应商广告检测逻辑

--- youtube-block/content.js
+++ youtube-block/content.js
@@ -1,10 +1,17 @@
 function checkForAds() {
     const adElements = document.querySelectorAll('.ad-showing');
     if (adElements.length > 0) {
-        location.reload();
+        location.replace(location.href);
+        return;
+    }
+
+    const sponsorAdButton = document.querySelector('.ytp-ad-overlay-close-button');
+    const skipButton = document.querySelector('.ytp-ad-skip-button');
+    if (sponsorAdButton || skipButton) {
+        location.replace(location.href);
     }
 }
 
-// 每 0.1 秒检查一次是否有广告
+// 每 0.1 秒（100 毫秒）检查一次是否有广告
 setInterval(checkForAds, 100);
     
